e7d990c4ddbbdb80cd5c58dc502e3889cbda74d7
Fauzan Azima <fauzanazima606@gmail.com>
2024-10-29 22:48:00 +0800

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-white bg-slate-600 font-mono text-3xl font-bold'>Setup tailwind + react vite project</h1>        
+    </>
+  )
+}
+
+export default App
