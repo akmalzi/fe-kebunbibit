edd8ccc33b339151d11414e25618d6c9c01c1ce3
Fauzan Azima <fauzanazima606@gmail.com>
2024-11-10 05:10:58 +0800

    refactor(ulasan): update ulasan layout to be responsive

--- src/components/Ulasan/UlasanList.jsx
+++ src/components/Ulasan/UlasanList.jsx
@@ -4,9 +4,11 @@ import dataUlasan from "../../data/dummy/ulasan-list.json"
 function UlasanList(){
     return(
         <>
-            {dataUlasan.map((item, index) => (
-                        <UlasanCard rating={item.rate} image={item.image} username={item.username} review={item.review} date={item.date} key={index}/>
-            ))}
+            <div className="py-4 px-6 border-l-[2px] w-full overflow-y-auto space-y-6 p-4">
+                {dataUlasan.map((item, index) => (
+                    <UlasanCard rating={item.rate} image={item.image} username={item.username} review={item.review} date={item.date} key={index}/>
+                ))}
+            </div> 
         </>
     )
 }
--- src/pages/Ulasan.jsx
+++ src/pages/Ulasan.jsx
@@ -1,27 +1,41 @@
+import { useState } from 'react';
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import UlasanList from "../components/Ulasan/UlasanList"
 import UlasanForm from "../components/Ulasan/UlasanForm"
-import CustomerRatingStars from "../components/Ulasan/CustomerRating"
 
 function Ulasan(){
+    const [activeSection, setActiveSection] = useState("ulasan-form"); // Keeps track of the active section
+
+    const togglePopup = (section) => {
+        setActiveSection(section === activeSection ? null : section)
+    };
+
     return(
         <>
             <Navbar/>
+            <ul className="lg:hidden flex mt-4">
+                <li><button className="mx-3 text-lg transition duration-200 ease-in text-black hover:text-primary-600" onClick={() => togglePopup("ulasan-form")}>Ulasan Form</button></li>
+                <li><button className="mx-3 text-lg transition duration-200 ease-in text-black hover:text-primary-600" onClick={() => togglePopup("ulasan-list")}>Ulasan List</button></li>
+            </ul>
             <section className="flex h-lvh bg-white animate-fade-in">
-                <div className="py-4 px-6 w-2/6">
-                    <div className="px-2 border-b-[1px] border-black max-w-fit">
-                        <h1 className="text-2xl font-medium">Ulasan</h1>
-                    </div>
-                    <div className="flex items-center mt-2">
-                        <h3 className="text-xl font-medium mr-2">4.9</h3>
-                        <CustomerRatingStars rating={4}/>
-                        <h3 className="text-xl font-medium ml-2">32 Ulasan</h3>
-                    </div>
+                <div className="hidden lg:block lg:w-2/6 min-h-lvh">
                     <UlasanForm/>
                 </div>
-                <div className="py-4 px-6 border-l-[2px] w-full md:w-2/3 overflow-y-auto space-y-6 p-4">
+                <div className="hidden lg:block lg:w-4/6 min-h-lvh overflow-y-auto">
                     <UlasanList/>
+                </div>
+                <div>
+                    {activeSection === "ulasan-form" && 
+                        <div className="w-screen lg:hidden h-lvh">
+                            <UlasanForm onClick={() => togglePopup("ulasan-form")} />
+                        </div>
+                    }
+                    {activeSection === "ulasan-list" && 
+                        <div className="w-full lg:hidden h-lvh overflow-y-auto">
+                            <UlasanList onClick={() => togglePopup("ulasan-list")}/>
+                        </div>
+                    }
                 </div>
             </section>
             <Footer/>
